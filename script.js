0b3ecc035261234f9c25c133b3c98c656befdf41
Laile005 <ac.side.job19@gmail.com>
2024-05-21 23:04:49 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
     const hamburger = document.getElementById('hamburger');
+    const closeMenu = document.getElementById('close-menu');
     const menu = document.getElementById('mobile_menu');
 
     hamburger.addEventListener('click', function() {
         menu.classList.toggle('show');
+        hamburger.classList.toggle('hidden');
+        closeMenu.classList.toggle('show');
+    });
+
+    closeMenu.addEventListener('click', function() {
+        menu.classList.remove('show');
+        hamburger.classList.remove('hidden');
+        closeMenu.classList.remove('show');
     });
 });
