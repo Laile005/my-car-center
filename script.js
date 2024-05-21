5a2ba989ab993536cd453ce156f210e9869a1bd1
Laile005 <ac.side.job19@gmail.com>
2024-05-21 21:57:36 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
     const hamburger = document.getElementById('hamburger');
+    const closeMenu = document.getElementById('close-menu');
     const menu = document.querySelector('nav ul');
 
     hamburger.addEventListener('click', function() {
         menu.classList.toggle('show');
+    });
+
+    closeMenu.addEventListener('click', function() {
+        menu.classList.remove('show');
     });
 });
