bb891681eeff8e430b03d4e607c693224ec45b04
Laile005 <ac.side.job19@gmail.com>
2024-05-21 23:26:37 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,16 +2,29 @@ document.addEventListener('DOMContentLoaded', function() {
     const hamburger = document.getElementById('hamburger');
     const closeMenu = document.getElementById('close-menu');
     const menu = document.getElementById('mobile_menu');
+    const body = document.body;
 
     hamburger.addEventListener('click', function() {
         menu.classList.toggle('show');
         hamburger.classList.add('hidden');
-        setTimeout(() => closeMenu.classList.add('show'), 300); // メニューが展開されると同時に×アイコンを表示
+        closeMenu.classList.add('show');
+        body.classList.add('no-scroll');
     });
 
     closeMenu.addEventListener('click', function() {
         menu.classList.remove('show');
         closeMenu.classList.remove('show');
-        setTimeout(() => hamburger.classList.remove('hidden'), 300); // メニューが閉じた後にハンバーガーアイコンを表示
+        hamburger.classList.remove('hidden');
+        body.classList.remove('no-scroll');
+    });
+
+    // メニュー外をクリックして閉じる
+    document.addEventListener('click', function(event) {
+        if (!menu.contains(event.target) && !hamburger.contains(event.target) && menu.classList.contains('show')) {
+            menu.classList.remove('show');
+            closeMenu.classList.remove('show');
+            hamburger.classList.remove('hidden');
+            body.classList.remove('no-scroll');
+        }
     });
 });
