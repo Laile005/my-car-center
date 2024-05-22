60d26e10d67b7d24dc012539a8a3f442761fa89f
Laile005 <ac.side.job19@gmail.com>
2024-05-22 12:12:18 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
     const hamburger = document.getElementById('hamburger');
     const closeMenu = document.getElementById('close-menu');
-    const menu = document.getElementById('mobile_menu');
+    const menu = document.getElementById('main_menu');
+    const nav = document.querySelector('nav');
     const body = document.body;
+
+    function checkScreenWidth() {
+        const screenWidth = window.innerWidth;
+
+        if (screenWidth < 1280) {
+            nav.classList.add('mobile');
+        } else {
+            nav.classList.remove('mobile');
+        }
+    }
+
+    window.addEventListener('resize', checkScreenWidth);
+    checkScreenWidth();
 
     hamburger.addEventListener('click', function() {
         menu.classList.toggle('show');
@@ -18,7 +32,6 @@ document.addEventListener('DOMContentLoaded', function() {
         body.classList.remove('no-scroll');
     });
 
-    // メニュー外をクリックして閉じる
     document.addEventListener('click', function(event) {
         if (!menu.contains(event.target) && !hamburger.contains(event.target) && menu.classList.contains('show')) {
             menu.classList.remove('show');
