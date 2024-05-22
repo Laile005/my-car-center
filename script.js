edb321917e892d180fc79ff76bbc5953a94aec20
Laile005 <ac.side.job19@gmail.com>
2024-05-22 12:56:19 +0900

    Update script.js

--- script.js
+++ script.js
@@ -5,25 +5,19 @@ document.addEventListener('DOMContentLoaded', function() {
     const body = document.body;
 
     hamburger.addEventListener('click', function() {
-        menu.classList.toggle('show');
-        hamburger.classList.add('hidden');
-        closeMenu.classList.add('show');
+        menu.classList.add('show');
         body.classList.add('no-scroll');
     });
 
     closeMenu.addEventListener('click', function() {
         menu.classList.remove('show');
-        closeMenu.classList.remove('show');
-        hamburger.classList.remove('hidden');
         body.classList.remove('no-scroll');
     });
 
     // メニュー外をクリックして閉じる
     document.addEventListener('click', function(event) {
-        if (!menu.contains(event.target) && !hamburger.contains(event.target) && menu.classList.contains('show')) {
+        if (!menu.contains(event.target) && event.target !== hamburger && menu.classList.contains('show')) {
             menu.classList.remove('show');
-            closeMenu.classList.remove('show');
-            hamburger.classList.remove('hidden');
             body.classList.remove('no-scroll');
         }
     });
