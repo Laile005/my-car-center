13bf57ea61a4115e1dd79cf5fe54f0b761d20f69
Laile005 <ac.side.job19@gmail.com>
2026-07-05 10:37:01 +0900

    Enable Microsoft Clarity tracking

--- analytics-config.js
+++ analytics-config.js
@@ -1,6 +1,6 @@
 window.MCC_ANALYTICS = {
   ga4Id: 'G-ZR46K2ME6D',
-  clarityId: '',
+  clarityId: 'xh8bpqs76a',
   enableScrollDepth: true,
   enableCtaTracking: true,
   disableClarityOnRecruit: true
